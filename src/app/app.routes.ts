59435650805b0ce1031f60aafd91f5c12d01212b
Sabina Liljeström <sabinaliljestrom@Sabinas-MacBook-Pro-2.local>
2024-05-29 20:24:50 +0200

    skapat meny och routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CoursesComponent } from './courses/courses.component';
+import { FramescheduleComponent } from './frameschedule/frameschedule.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'home', component: HomeComponent},
+    {path: 'courses', component: CoursesComponent},
+    {path: 'frameschedule', component: FramescheduleComponent},
+    {path: '', redirectTo: '/home', pathMatch: 'full'},
+];
