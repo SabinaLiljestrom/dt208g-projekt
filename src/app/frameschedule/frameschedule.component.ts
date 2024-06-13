5498849c182e901ab075455d535d4a4a69667e6d
Sabina Liljeström <sabinaliljestrom@Sabinas-MacBook-Pro-2.local>
2024-06-13 15:29:02 +0200

    skapat en footer, skapat en service som hämtar data från localstorage, uppdaterat komponent för ramschema så att kurser som är sparade i local storage läses ut på sidan och uppdateras om nya läggs till

--- src/app/frameschedule/frameschedule.component.ts
+++ src/app/frameschedule/frameschedule.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Course } from '../models/course';
+import { FramescheduleService } from '../services/frameschedule.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-frameschedule',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './frameschedule.component.html',
   styleUrl: './frameschedule.component.scss'
 })
-export class FramescheduleComponent {
+export class FramescheduleComponent implements OnInit {
+  savedCourses: Course[] = [];
+  constructor(private FramescheduleService: FramescheduleService) {}
 
+  ngOnInit(): void {
+    this.FramescheduleService.getCourses().subscribe(courses => {
+      this.savedCourses = courses;
+    });
+  }
 }
--- src/app/services/frameschedule.service.ts
+++ src/app/services/frameschedule.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { Course } from '../models/course';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FramescheduleService {
+  private storageKey = 'savedCourses';
+  private coursesSubject = new BehaviorSubject<Course[]>(this.getCoursesFromLocalStorage());
 
   constructor() { }
+  private getCoursesFromLocalStorage(): Course[] {
+    const data = localStorage.getItem(this.storageKey);
+    return data ? JSON.parse(data) : [];
+}
+getCourses() {
+  return this.coursesSubject.asObservable();
+}
+
+addCourse(course: Course) {
+  const courses = this.getCoursesFromLocalStorage();
+  courses.push(course);
+  localStorage.setItem(this.storageKey, JSON.stringify(courses));
+  this.coursesSubject.next(courses); // Uppdatera observablen
+}
 }
