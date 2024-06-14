3a24c6a5669d1ba70ff3d442c7b00f51138c6f2b
Sabina Liljeström <sabinaliljestrom@Sabinas-MacBook-Pro-2.local>
2024-06-14 20:33:08 +0200

    uppdaterat hela tabellen till mat-table för att kunna använda egenskaper för räkningen av poängen och uppdaterat komponenten och servicen för ramschemat

--- src/app/frameschedule/frameschedule.component.ts
+++ src/app/frameschedule/frameschedule.component.ts
@@ -2,21 +2,26 @@ import { Component, OnInit } from '@angular/core';
 import { Course } from '../models/course';
 import { FramescheduleService } from '../services/frameschedule.service';
 import { CommonModule } from '@angular/common';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-frameschedule',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatTableModule],
   templateUrl: './frameschedule.component.html',
   styleUrl: './frameschedule.component.scss'
 })
 export class FramescheduleComponent implements OnInit {
   savedCourses: Course[] = [];
+  totalPoints: number = 0;
   constructor(private FramescheduleService: FramescheduleService) {}
 
   ngOnInit(): void {
     this.FramescheduleService.getCourses().subscribe(courses => {
       this.savedCourses = courses;
+    });
+    this.FramescheduleService.getTotalPoints().subscribe(points => {
+      this.totalPoints = points;
     });
   }
   removeCourse(courseCode: string) {
--- src/app/services/frameschedule.service.ts
+++ src/app/services/frameschedule.service.ts
@@ -8,26 +8,40 @@ import { Course } from '../models/course';
 export class FramescheduleService {
   private storageKey = 'savedCourses';
   private coursesSubject = new BehaviorSubject<Course[]>(this.getCoursesFromLocalStorage());
+  private totalPointsSubject = new BehaviorSubject<number>(this.calculateTotalPoints(this.getCoursesFromLocalStorage()));
 
-  constructor() { }
+  constructor() {}
+
   private getCoursesFromLocalStorage(): Course[] {
     const data = localStorage.getItem(this.storageKey);
     return data ? JSON.parse(data) : [];
-}
-getCourses() {
-  return this.coursesSubject.asObservable();
-}
+  }
 
-addCourse(course: Course) {
-  const courses = this.getCoursesFromLocalStorage();
-  courses.push(course);
-  localStorage.setItem(this.storageKey, JSON.stringify(courses));
-  this.coursesSubject.next(courses); // Uppdatera observablen
-}
-removeCourse(courseCode: string) {
-  let courses = this.getCoursesFromLocalStorage();
-  courses = courses.filter(course => course.courseCode !== courseCode);
-  localStorage.setItem(this.storageKey, JSON.stringify(courses));
-  this.coursesSubject.next(courses); // Uppdatera observablen
-}
+  private calculateTotalPoints(courses: Course[]): number {
+    return courses.reduce((sum, course) => sum + course.points, 0);
+  }
+
+  getCourses() {
+    return this.coursesSubject.asObservable();
+  }
+
+  getTotalPoints() {
+    return this.totalPointsSubject.asObservable();
+  }
+
+  addCourse(course: Course) {
+    const courses = this.getCoursesFromLocalStorage();
+    courses.push(course);
+    localStorage.setItem(this.storageKey, JSON.stringify(courses));
+    this.coursesSubject.next(courses); // Uppdatera observablen
+    this.totalPointsSubject.next(this.calculateTotalPoints(courses)); // Uppdatera poäng
+  }
+
+  removeCourse(courseCode: string) {
+    let courses = this.getCoursesFromLocalStorage();
+    courses = courses.filter(course => course.courseCode !== courseCode);
+    localStorage.setItem(this.storageKey, JSON.stringify(courses));
+    this.coursesSubject.next(courses); // Uppdatera observablen
+    this.totalPointsSubject.next(this.calculateTotalPoints(courses)); // Uppdatera poäng
+  }
 }
