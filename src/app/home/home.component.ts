64f24607195bae4caaa276136ab69cec857cc493
Sabina Liljeström <sabinaliljestrom@Sabinas-MacBook-Pro-2.local>
2024-06-16 21:37:46 +0200

    gjort ramschemat responsivt, uppdaterat footer och home komponent utseendemässigt

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,5 +8,8 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  campus: string ="assets/images/campus.jpg";
+  books: string ="assets/images/books.png";
+  students: string ="assets/images/students.png";
+  gym: string ="assets/images/gym.png";
 }
